dd35b60e444e6d61fb5347c757cf3cd957856d4a
inhyeeeok <92982812+inhyeeeok@users.noreply.github.com>
2023-03-14 17:00:58 +0900

    MySQL 접속 및 쿼리 실행 / 연결 close 샘플

---
+++ test.js
@@ -0,0 +1,15 @@
+console.log('test');
+
+
+// MySQL 접속 및 쿼리 실행 / 연결 close
+var db_config = require(__dirname + '/sql.js');
+var conn = db_config.init();
+
+const sql = "select * from test_table";
+
+db_config.query(conn, sql, function (err, res) {
+    console.log(err, res);
+});
+
+
+
