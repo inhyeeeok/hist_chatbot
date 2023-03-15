129845db908afcf69b20b8789d48a5642dfeda46
inhyeeeok <92982812+inhyeeeok@users.noreply.github.com>
2023-03-15 16:34:12 +0900

    월요일~금요일 선택 시 전달 값 샘플 commit

--- salad.js
+++ salad.js
@@ -10,103 +10,108 @@
 
 //Onput Reservation Data Example
 const inputTestJson = {
-    "날짜": "{\"from\": {\"date\": \"2023-03-13\", \"dateTag\": null, \"dateHeadword\": null, \"year\": null, \"month\": null, \"day\": null}, \"to\": {\"date\": \"2023-03-19\", \"dateTag\": null, \"dateHeadword\": null, \"year\": null, \"month\": null, \"day\": null}, \"polynomial\": \"current_week\", \"calendar_type\": \"solar\", \"emp_no\": \"20210013\"}"
+  "날짜": "{\"from\": {\"date\": \"2023-03-13\", \"dateTag\": null, \"dateHeadword\": null, \"year\": null, \"month\": null, \"day\": null}, \"to\": {\"date\": \"2023-03-19\", \"dateTag\": null, \"dateHeadword\": null, \"year\": null, \"month\": null, \"day\": null}, \"polynomial\": \"current_week\", \"calendar_type\": \"solar\", \"emp_no\": \"20210013\"}"
 }
+
+const inputSampleTestJson = {
+  "날짜": "{\"date\": \"2023-03-13\", \"dateTag\": \"Monday\", \"dateHeadword\": null, \"year\": null, \"month\": null, \"day\": null, \"polynomial\": \"define_weekday({0})\", \"calendar_type\": \"solar\"}"
+}
+
 
 //Output Reservation Data Example (https://i.kakao.com/docs/skill-response-format#skillpayload)
 const responseTestJson = {
-    "version": "2.0",
-    "template": {
-      "outputs": [
-          {
-              "simpleText": {
-                  "text": "예약이 성공적으로 완료되었습니다. 신청하신 예약을 확인해 주세요."
-              }
-            },
-        {
-          "carousel": {
-            "type": "itemCard",
-            "items": [
-              {
-                "imageTitle": {
-                  "title": "예약 완료",
-                  "imageUrl" : "https://t1.kakaocdn.net/openbuilder/docs_image/wine.jpg"
+  "version": "2.0",
+  "template": {
+    "outputs": [
+      {
+        "simpleText": {
+          "text": "예약이 성공적으로 완료되었습니다. 신청하신 예약을 확인해 주세요."
+        }
+      },
+      {
+        "carousel": {
+          "type": "itemCard",
+          "items": [
+            {
+              "imageTitle": {
+                "title": "예약 완료",
+                "imageUrl": "https://t1.kakaocdn.net/openbuilder/docs_image/wine.jpg"
+              },
+              "itemList": [
+                {
+                  "title": "매장명",
+                  "description": "대한항공 인재개발원"
                 },
-                "itemList": [
-                  {
-                    "title": "매장명",
-                    "description": "대한항공 인재개발원"
-                  },
-                  {
-                    "title": "예약 일자",
-                    "description": "2022.12.25, 19:30"
-                  },
-                  {
-                    "title": "예약 일시",
-                    "description": "2022.12.25, 19:30"
-                  },
-                  {
-                    "title" : "예약 내용",
-                    "description" : "샐러드 신청"
-                  }
-                ],
-                "itemListAlignment": "left",
-                "buttons": [
-                  {
-                    "label": "예약 내역",
-                    "action": "message",
-                    "messageText" : "예약 내역"
-                  },
-                  {
-                    "label": "예약 취소",
-                    "action": "message",
-                    "messageText": "예약 취소"
-                  }
-                ]
-              }
-            ]
-          }
+                {
+                  "title": "예약 일자",
+                  "description": "2022.12.25, 19:30"
+                },
+                {
+                  "title": "예약 일시",
+                  "description": "2022.12.25, 19:30"
+                },
+                {
+                  "title": "예약 내용",
+                  "description": "샐러드 신청"
+                }
+              ],
+              "itemListAlignment": "left",
+              "buttons": [
+                {
+                  "label": "예약 내역",
+                  "action": "message",
+                  "messageText": "예약 내역"
+                },
+                {
+                  "label": "예약 취소",
+                  "action": "message",
+                  "messageText": "예약 취소"
+                }
+              ]
+            }
+          ]
         }
-      ],
-      "quickReplies": [
-        {
-          "messageText": "처음으로",
-          "action": "message",
-          "label": "메뉴"
-        },
-        {
-          "messageText": "도움말",
-          "action": "message",
-          "label": "도움말"
-        },
-        {
-          "messageText": "기타",
-          "action": "message",
-          "label": "기타"
-        }
-      ]
-    }
-  };
+      }
+    ],
+    "quickReplies": [
+      {
+        "messageText": "처음으로",
+        "action": "message",
+        "label": "메뉴"
+      },
+      {
+        "messageText": "도움말",
+        "action": "message",
+        "label": "도움말"
+      },
+      {
+        "messageText": "기타",
+        "action": "message",
+        "label": "기타"
+      }
+    ]
+  }
+};
 
 // Reservation Function
 const reservation = inputTestJson => {
-  
+
   var input = JSON.parse(inputTestJson.날짜);
   var emp_no = '';
   var from_date = '';
   var to_date = '';
 
-  if(input.emp_no !== null){
+  if (input.emp_no !== null) {
     emp_no = input.emp_no;
     console.log(emp_no);
   }
 
-  if(input.from.date !== null){
+  if (input.from.date !== null) {
     from_date = input.from.date;
     console.log(from_date);
   }
 
-  if(input.to.date !== null){
+  if (input.to.date !== null) {
     to_date = input.to.date;
     console.log(to_date);
   }
